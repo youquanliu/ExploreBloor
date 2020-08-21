621a36414d8601e4de0fc402642604b330aaf2e4
unknown <youquan.liu@hotmail.com>
2020-08-21 10:54:04 -0400

    change

--- models/comment.js
+++ models/comment.js
@@ -3,13 +3,6 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
     text: String,
-    author: {
-        id: {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
-    }
 }, {
     timestamps: true
 });
--- models/posts.js
+++ models/posts.js
@@ -6,13 +6,7 @@ const postsSchema = new Schema({
     name: String,
     image: String,
     description: String,
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
-    },
+
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment"
